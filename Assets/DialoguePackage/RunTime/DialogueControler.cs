42830d99416c4d176fc5dc109f2e13363f94cb12
AlexandreSanchesMateus <90258109+AlexandreSanchesMateus@users.noreply.github.com>
2023-01-19 10:36:55 +0100

    Fix DoTween import

--- Assets/DialoguePackage/RunTime/DialogueControler.cs
+++ Assets/DialoguePackage/RunTime/DialogueControler.cs
@@ -81,6 +81,7 @@ public class DialogueControler : MonoBehaviour
                     sentences.Enqueue(other.sentence.FR);
                 }
 
+
                 DisplayNextSentence();
                 break;
 
