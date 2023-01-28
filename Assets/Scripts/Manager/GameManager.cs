e7797e1065f28a5e784301ee411c406efaba7c1e
AlexandreSanchesMateus <90258109+AlexandreSanchesMateus@users.noreply.github.com>
2023-01-28 15:07:20 +0100

    settings fonctionnel

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -14,9 +14,16 @@ namespace TeamSeven
 
         [Header("Audio Settings")]
         public AudioMixer audioMixer;
+        public AnimationCurve audioSettingsCurve;
 
         public delegate void LanguageListner();
         public LanguageListner OnLanguageChange;
+
+
+        private const string MUSIC_VOLUME = "MusicVolume";
+        private const string SFX_VOLUME = "SfxVolume";
+        private const string VOICE_VOLUME = "VoiceVolume";
+
 
         [System.Serializable]
         public enum LANGUAGE
@@ -50,6 +57,36 @@ namespace TeamSeven
         public void GoToSnapshot(string name, float time = 2)
         {
             audioMixer.FindSnapshot(name)?.TransitionTo(time);
+        }
+
+        public void OnMusicVolumeChange(float value)
+        {
+            if (value == 0)
+                audioMixer.SetFloat(MUSIC_VOLUME, 0);
+            else
+                audioMixer.SetFloat(MUSIC_VOLUME, ParseToDebit20(value));
+        }
+
+        public void OnSfxVolumeChange(float value)
+        {
+            if (value == 0)
+                audioMixer.SetFloat(SFX_VOLUME, 0);
+            else
+                audioMixer.SetFloat(SFX_VOLUME, ParseToDebit20(value));
+        }
+
+        public void OnVoiceVolumeChange(float value)
+        {
+            if (value == 0)
+                audioMixer.SetFloat(VOICE_VOLUME, 0);
+            else
+                audioMixer.SetFloat(VOICE_VOLUME, ParseToDebit20(value));
+        }
+
+        private float ParseToDebit20(float value)
+        {
+            float parse = Mathf.Lerp(-80, 20, audioSettingsCurve.Evaluate(Mathf.Clamp01(value)));
+            return parse;
         }
     }
 }
